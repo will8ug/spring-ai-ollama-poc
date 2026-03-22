f11dc160aacb216cb174e02870a32b698f56e4d8
Will <yzj_000@163.com>
2026-03-22 15:04:13 +0800

    Initial commit

---
+++ src/test/java/io/will/springai2poc/controller/PingControllerIT.java
@@ -0,0 +1,31 @@
+package io.will.springai2poc.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PingControllerIT {
+
+    @Value("${local.server.port}")
+    private int port;
+
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    void beforeEach() {
+        webTestClient = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .build();
+    }
+
+    @Test
+    void pingReturnsPong() {
+        webTestClient.get().uri("/ping")
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(String.class).isEqualTo("PONG");
+    }
+}
